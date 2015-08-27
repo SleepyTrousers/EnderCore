90135339b04c7fe9b26c500d35e35e079ce6815b
tterrag1098 <tterrag1098@gmail.com>
2015-08-27 02:11:19 -0400

    Add CustomWidgetIcon

---
+++ src/main/java/com/enderio/core/client/render/CustomWidgetIcon.java
@@ -0,0 +1,20 @@
+package com.enderio.core.client.render;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import com.enderio.core.api.client.render.IWidgetIcon;
+import com.enderio.core.api.client.render.IWidgetMap;
+
+@Getter
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class CustomWidgetIcon implements IWidgetIcon {
+
+  private final int x, y, width, height;
+  private final IWidgetMap map;
+  
+  private IWidgetIcon overlay;
+
+}
