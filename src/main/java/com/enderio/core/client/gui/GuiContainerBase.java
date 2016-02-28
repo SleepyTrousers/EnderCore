1a64e5ee52ed8aab4602ab3559263e2bac55a92f
tterrag1098 <tterrag1098@gmail.com>
2016-02-27 20:15:22 -0500

    SleepyTrousers/EnderIO#3184 Fix weird rendering with overlays/stacks

--- src/main/java/com/enderio/core/client/gui/GuiContainerBase.java
+++ src/main/java/com/enderio/core/client/gui/GuiContainerBase.java
@@ -13,7 +13,6 @@ import net.minecraft.client.renderer.RenderHelper;
 import net.minecraft.inventory.Container;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Timer;
-import net.minecraftforge.client.ForgeHooksClient;
 import net.minecraftforge.common.ForgeHooks;
 
 import org.lwjgl.input.Mouse;
@@ -32,7 +31,6 @@ import com.enderio.core.client.gui.widget.GhostSlot;
 import com.enderio.core.client.gui.widget.GuiToolTip;
 import com.enderio.core.client.gui.widget.TextFieldEnder;
 import com.enderio.core.client.gui.widget.VScrollbar;
-import com.enderio.core.client.render.EnderWidget;
 import com.enderio.core.client.render.RenderUtil;
 import com.google.common.collect.Lists;
 
@@ -355,14 +353,7 @@ public abstract class GuiContainerBase extends GuiContainer implements ToolTipRe
   public void drawScreen(int par1, int par2, float par3) {
     int mx = realMx = par1;
     int my = realMy = par2;
-    for (IGuiOverlay overlay : overlays) {
-      if (overlay != null && overlay.isVisible() && isMouseInOverlay(par1, par2, overlay)) {
-        mx = -5000;
-        my = -5000;
-        this.drawItemStack(this.mc.thePlayer.inventory.getItemStack(), par1 - this.guiLeft - 8, par2 - this.guiTop - 8, null);
-      }
-    }
-
+    
     super.drawScreen(mx, my, par3);
 
     if (draggingScrollbar == null) {
@@ -450,12 +441,6 @@ public abstract class GuiContainerBase extends GuiContainer implements ToolTipRe
   @Deprecated
   protected GhostSlot getGhostSlot(int mouseX, int mouseY) {
     return ghostSlotHandler.getGhostSlot(this, mouseX, mouseY);
-  }
-
-  private boolean isMouseInOverlay(int mouseX, int mouseY, IGuiOverlay overlay) {
-    int x = mouseX - getGuiLeft();
-    int y = mouseY - getGuiTop();
-    return overlay.getBounds().contains(x, y);
   }
 
   @Override
