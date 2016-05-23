695136fb284aeeef3a83011ea2d2f3f20a6052b2
Henry Loenwind <henry@loenwind.info>
2016-05-23 02:59:02 +0200

    More annotation madness

--- src/main/java/com/enderio/core/client/gui/GuiContainerBase.java
+++ src/main/java/com/enderio/core/client/gui/GuiContainerBase.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
 
+import javax.annotation.Nonnull;
+
 import org.lwjgl.input.Mouse;
 
 import com.enderio.core.api.client.gui.IGuiOverlay;
@@ -412,7 +414,7 @@ public abstract class GuiContainerBase extends GuiContainer implements ToolTipRe
   }
 
   @Override
-  public void renderToolTip(ItemStack p_146285_1_, int p_146285_2_, int p_146285_3_) {
+  public void renderToolTip(@Nonnull ItemStack p_146285_1_, int p_146285_2_, int p_146285_3_) {
     super.renderToolTip(p_146285_1_, p_146285_2_, p_146285_3_);
   }
 
@@ -451,7 +453,7 @@ public abstract class GuiContainerBase extends GuiContainer implements ToolTipRe
 
   @SuppressWarnings({ "rawtypes", "unchecked" })
   @Override
-  public void drawHoveringText(List par1List, int par2, int par3, FontRenderer font) {
+  public void drawHoveringText(List<String> par1List, int par2, int par3, FontRenderer font) {
     copyOfdrawHoveringText(par1List, par2, par3, font);   
   }
 
