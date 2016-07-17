87d79bf1b525e2628334760d2a12802654c33896
Henry Loenwind <henry@loenwind.info>
2016-07-17 14:40:15 +0200

    Reinstated allowExternalTickSpeedup config and fixed formatting

--- src/main/java/com/enderio/core/common/TileEntityBase.java
+++ src/main/java/com/enderio/core/common/TileEntityBase.java
@@ -1,6 +1,7 @@
 package com.enderio.core.common;
 
 import com.enderio.core.api.common.util.IProgressTile;
+import com.enderio.core.common.config.ConfigHandler;
 import com.enderio.core.common.network.EnderPacketHandler;
 import com.enderio.core.common.network.PacketProgress;
 
@@ -17,151 +18,155 @@ import net.minecraft.world.World;
 
 public abstract class TileEntityBase extends TileEntity implements ITickable {
 
-    private final int checkOffset = (int) (Math.random() * 20);
-    protected final boolean isProgressTile;
+  private final int checkOffset = (int) (Math.random() * 20);
+  protected final boolean isProgressTile;
 
-    protected int lastProgressScaled = -1;
-    protected int ticksSinceLastProgressUpdate;
+  protected int lastProgressScaled = -1;
+  protected int ticksSinceLastProgressUpdate;
+  private long lastUpdate = 0;
 
-    public TileEntityBase() {
-        isProgressTile = this instanceof IProgressTile;
-    }
+  public TileEntityBase() {
+    isProgressTile = this instanceof IProgressTile;
+  }
 
-    @Override
-    public final void update() {
-        doUpdate();
-        if (isProgressTile && !worldObj.isRemote) {
-            int curScaled = getProgressScaled(16);
-            if (++ticksSinceLastProgressUpdate >= getProgressUpdateFreq() || curScaled != lastProgressScaled) {
-                sendTaskProgressPacket();
-                lastProgressScaled = curScaled;
-            }
+  @Override
+  public final void update() {
+    if (ConfigHandler.allowExternalTickSpeedup || worldObj.getTotalWorldTime() != lastUpdate) {
+      lastUpdate = worldObj.getTotalWorldTime();
+      doUpdate();
+      if (isProgressTile && !worldObj.isRemote) {
+        int curScaled = getProgressScaled(16);
+        if (++ticksSinceLastProgressUpdate >= getProgressUpdateFreq() || curScaled != lastProgressScaled) {
+          sendTaskProgressPacket();
+          lastProgressScaled = curScaled;
         }
+      }
     }
+  }
 
-    public static int getProgressScaled(int scale, IProgressTile tile) {
-        return (int) (tile.getProgress() * scale);
+  public static int getProgressScaled(int scale, IProgressTile tile) {
+    return (int) (tile.getProgress() * scale);
+  }
+
+  public final int getProgressScaled(int scale) {
+    if (isProgressTile) {
+      return getProgressScaled(scale, (IProgressTile) this);
     }
+    return 0;
+  }
 
-    public final int getProgressScaled(int scale) {
-        if (isProgressTile) {
-            return getProgressScaled(scale, (IProgressTile) this);
-        }
-        return 0;
+  protected void doUpdate() {
+
+  }
+
+  protected void sendTaskProgressPacket() {
+    if (isProgressTile) {
+      EnderPacketHandler.sendToAllAround(new PacketProgress((IProgressTile) this), this);
     }
+    ticksSinceLastProgressUpdate = 0;
+  }
 
-    protected void doUpdate() {
+  /**
+   * Controls how often progress updates. Has no effect if your TE is not {@link IProgressTile}.
+   */
+  protected int getProgressUpdateFreq() {
+    return 20;
+  }
 
+  @Override
+  public final void readFromNBT(NBTTagCompound root) {
+    super.readFromNBT(root);
+    readCustomNBT(root);
+  }
+
+  @Override
+  public final NBTTagCompound writeToNBT(NBTTagCompound root) {
+    super.writeToNBT(root);
+    writeCustomNBT(root);
+    return root;
+  }
+
+  @Override
+  public NBTTagCompound getUpdateTag() {
+    NBTTagCompound tag = new NBTTagCompound();
+    writeCustomNBT(tag);
+    return tag;
+  }
+
+  @Override
+  public SPacketUpdateTileEntity getUpdatePacket() {
+    NBTTagCompound tag = new NBTTagCompound();
+    writeCustomNBT(tag);
+    return new SPacketUpdateTileEntity(getPos(), 1, tag);
+  }
+
+  @Override
+  public void onDataPacket(NetworkManager net, SPacketUpdateTileEntity pkt) {
+    readCustomNBT(pkt.getNbtCompound());
+  }
+
+  public boolean canPlayerAccess(EntityPlayer player) {
+    return !isInvalid() && player.getDistanceSqToCenter(getPos().add(0.5, 0.5, 0.5)) <= 64D;
+  }
+
+  protected abstract void writeCustomNBT(NBTTagCompound root);
+
+  protected abstract void readCustomNBT(NBTTagCompound root);
+
+  protected void updateBlock() {
+    if (worldObj != null) {
+      IBlockState bs = worldObj.getBlockState(getPos());
+      worldObj.notifyBlockUpdate(pos, bs, bs, 3);
     }
+  }
 
-    protected void sendTaskProgressPacket() {
-        if (isProgressTile) {
-          EnderPacketHandler.sendToAllAround(new PacketProgress((IProgressTile) this), this);
-        }
-        ticksSinceLastProgressUpdate = 0;
-    }
-
-    /**
-     * Controls how often progress updates. Has no effect if your TE is not {@link IProgressTile}.
-     */
-    protected int getProgressUpdateFreq() {
-        return 20;
-    }
-
-    @Override
-    public final void readFromNBT(NBTTagCompound root) {
-        super.readFromNBT(root);
-        readCustomNBT(root);
-    }
-
-    @Override
-    public final NBTTagCompound writeToNBT(NBTTagCompound root) {
-        super.writeToNBT(root);
-        writeCustomNBT(root);
-        return root;
-    }
-
-    @Override
-    public NBTTagCompound getUpdateTag() {
-      NBTTagCompound tag = new NBTTagCompound();
-      writeCustomNBT(tag);
-      return tag;
-    }    
-    
-    @Override
-    public SPacketUpdateTileEntity getUpdatePacket() {
-        NBTTagCompound tag = new NBTTagCompound();
-        writeCustomNBT(tag);
-        return new SPacketUpdateTileEntity(getPos(), 1, tag);
-    }
-
-    @Override
-    public void onDataPacket(NetworkManager net, SPacketUpdateTileEntity pkt) {
-        readCustomNBT(pkt.getNbtCompound());
-    }
-
-    public boolean canPlayerAccess(EntityPlayer player) {
-        return !isInvalid() && player.getDistanceSqToCenter(getPos().add(0.5, 0.5, 0.5)) <= 64D;
-    }
-
-    protected abstract void writeCustomNBT(NBTTagCompound root);
-
-    protected abstract void readCustomNBT(NBTTagCompound root);
-
-    protected void updateBlock() {
-        if (worldObj != null) {
-          IBlockState bs = worldObj.getBlockState(getPos());
-          worldObj.notifyBlockUpdate(pos, bs, bs, 3);          
-        }
-    }
-
-    protected boolean isPoweredRedstone() {
+  protected boolean isPoweredRedstone() {
     return worldObj.isBlockLoaded(getPos()) ? worldObj.isBlockIndirectlyGettingPowered(getPos()) > 0 : false;
-    }
+  }
 
-    /**
-     * Called directly after the TE is constructed. This is the place to call non-final methods.
-     *
-     * Note: This will not be called when the TE is loaded from the save. Hook into the nbt methods for that.
-     */
-    public void init() {}
+  /**
+   * Called directly after the TE is constructed. This is the place to call non-final methods.
+   *
+   * Note: This will not be called when the TE is loaded from the save. Hook into the nbt methods for that.
+   */
+  public void init() {
+  }
 
-    /**
-     * Call this with an interval (in ticks) to find out if the current tick is the one you want to do some work. This
-     * is staggered so the work of different TEs is stretched out over time.
-     *
-     * @see #shouldDoWorkThisTick(int, int) If you need to offset work ticks
-     */
-    protected boolean shouldDoWorkThisTick(int interval) {
-        return shouldDoWorkThisTick(interval, 0);
-    }
+  /**
+   * Call this with an interval (in ticks) to find out if the current tick is the one you want to do some work. This is staggered so the work of different TEs
+   * is stretched out over time.
+   *
+   * @see #shouldDoWorkThisTick(int, int) If you need to offset work ticks
+   */
+  protected boolean shouldDoWorkThisTick(int interval) {
+    return shouldDoWorkThisTick(interval, 0);
+  }
 
-    /**
-     * Call this with an interval (in ticks) to find out if the current tick is the one you want to do some work. This
-     * is staggered so the work of different TEs is stretched out over time.
-     *
-     * If you have different work items in your TE, use this variant to stagger your work.
-     */
-    protected boolean shouldDoWorkThisTick(int interval, int offset) {
-        return (worldObj.getTotalWorldTime() + checkOffset + offset) % interval == 0;
-    }
+  /**
+   * Call this with an interval (in ticks) to find out if the current tick is the one you want to do some work. This is staggered so the work of different TEs
+   * is stretched out over time.
+   *
+   * If you have different work items in your TE, use this variant to stagger your work.
+   */
+  protected boolean shouldDoWorkThisTick(int interval, int offset) {
+    return (worldObj.getTotalWorldTime() + checkOffset + offset) % interval == 0;
+  }
 
-    @Override
-    public boolean shouldRefresh(World world, BlockPos pos, IBlockState oldState, IBlockState newSate) {
-        return oldState.getBlock() != newSate.getBlock();
-    }
-    
-    /**
-     * Called server-side when a GhostSlot is changed. Check that the given slot
-     * number really is a ghost slot before storing the given stack.
-     * 
-     * @param slot
-     *          The slot number that was given to the ghost slot
-     * @param stack
-     *          The stack that should be placed, null to clear
-     */
-    public void setGhostSlotContents(int slot, ItemStack stack) {
-    }
+  @Override
+  public boolean shouldRefresh(World world, BlockPos pos, IBlockState oldState, IBlockState newSate) {
+    return oldState.getBlock() != newSate.getBlock();
+  }
+
+  /**
+   * Called server-side when a GhostSlot is changed. Check that the given slot number really is a ghost slot before storing the given stack.
+   * 
+   * @param slot
+   *          The slot number that was given to the ghost slot
+   * @param stack
+   *          The stack that should be placed, null to clear
+   */
+  public void setGhostSlotContents(int slot, ItemStack stack) {
+  }
 
   @Override
   public void markDirty() {
