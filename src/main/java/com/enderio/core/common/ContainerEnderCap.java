ab9aee29d12eb88e2ed8c1b218dd2797cee185b2
CrazyPants <crazypants.mc@gmail.com>
2016-08-28 01:17:35 +0800

    Added capability based Container base class.

---
+++ src/main/java/com/enderio/core/common/ContainerEnderCap.java
@@ -0,0 +1,208 @@
+package com.enderio.core.common;
+
+import java.awt.Point;
+import java.util.Map;
+
+import javax.annotation.Nonnull;
+
+import com.enderio.core.client.gui.widget.GhostSlot;
+import com.google.common.collect.Maps;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.inventory.Container;
+import net.minecraft.inventory.Slot;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.items.IItemHandler;
+
+public abstract class ContainerEnderCap<T extends IItemHandler> extends Container implements GhostSlot.IGhostSlotAware {
+  
+  protected Map<Slot, Point> playerSlotLocations = Maps.newLinkedHashMap();
+  
+  protected final int startPlayerSlot;
+  protected final int endPlayerSlot;
+  protected final int startHotBarSlot;
+  protected final int endHotBarSlot;
+  
+  private final @Nonnull T inv;
+  private final @Nonnull InventoryPlayer playerInv;  
+  
+  @Nonnull
+  private static <T> T checkNotNull(T reference) {
+    if (reference == null) {
+      throw new NullPointerException();
+    }
+    return reference;
+  }
+  
+  public ContainerEnderCap(InventoryPlayer playerInv, T itemHandler) {
+    inv = checkNotNull(itemHandler);
+    this.playerInv = checkNotNull(playerInv);    
+    addSlots();
+
+    int x = getPlayerInventoryOffset().x;
+    int y = getPlayerInventoryOffset().y;
+
+    // add players inventory
+    startPlayerSlot = inventorySlots.size();
+    for (int i = 0; i < 3; ++i) {
+      for (int j = 0; j < 9; ++j) {
+        Point loc = new Point(x + j * 18, y + i * 18);
+        Slot slot = new Slot(playerInv, j + i * 9 + 9, loc.x, loc.y);
+        addSlotToContainer(slot);
+        playerSlotLocations.put(slot, loc);
+      }
+    }
+    endPlayerSlot = inventorySlots.size();
+
+    startHotBarSlot = inventorySlots.size();
+    for (int i = 0; i < 9; ++i) {
+      Point loc = new Point(x + i * 18, y + 58);
+      Slot slot = new Slot(playerInv, i, loc.x, loc.y);
+      addSlotToContainer(slot);
+      playerSlotLocations.put(slot, loc);
+    }
+    endHotBarSlot = inventorySlots.size();
+  }
+    
+  public Point getPlayerInventoryOffset() {    
+    return new Point(0, 54);
+  }   
+
+  @Nonnull 
+  public T getItemHandler() {
+    return inv;
+  }
+
+  @Override
+  public boolean canInteractWith(EntityPlayer player) {
+    //TODO: How do I do this with caps?  
+    return true;
+  }
+  
+  protected abstract void addSlots();
+  
+  @Override
+  public void setGhostSlotContents(int slot, ItemStack stack) {
+    if (inv instanceof TileEntityBase) {
+      ((TileEntityBase) inv).setGhostSlotContents(slot, stack);
+    }
+
+  }
+  
+  @Override
+  public ItemStack transferStackInSlot(EntityPlayer p_82846_1_, int p_82846_2_) {
+    ItemStack itemstack = null;
+    Slot slot = this.inventorySlots.get(p_82846_2_);
+
+    if (slot != null && slot.getHasStack()) {
+      ItemStack itemstack1 = slot.getStack();
+      itemstack = itemstack1.copy();
+
+      int minPlayerSlot = inventorySlots.size() - playerInv.mainInventory.length;
+      if (p_82846_2_ < minPlayerSlot) {
+        if (!this.mergeItemStack(itemstack1, minPlayerSlot, this.inventorySlots.size(), true)) {
+          return null;
+        }
+      } else if (!this.mergeItemStack(itemstack1, 0, minPlayerSlot, false)) {
+        return null;
+      }
+
+      if (itemstack1.stackSize == 0) {
+        slot.putStack((ItemStack) null);
+      } else {
+        slot.onSlotChanged();
+      }
+    }
+
+    return itemstack;
+  }
+
+  /**
+   * Added validation of slot input
+   */
+  @Override
+  protected boolean mergeItemStack(ItemStack par1ItemStack, int fromIndex, int toIndex, boolean reversOrder) {
+
+    boolean result = false;
+    int checkIndex = fromIndex;
+
+    if (reversOrder) {
+      checkIndex = toIndex - 1;
+    }
+
+    Slot slot;
+    ItemStack itemstack1;
+
+    if (par1ItemStack.isStackable()) {
+
+      while (par1ItemStack.stackSize > 0 && (!reversOrder && checkIndex < toIndex || reversOrder && checkIndex >= fromIndex)) {
+        slot = this.inventorySlots.get(checkIndex);
+        itemstack1 = slot.getStack();
+
+        if (itemstack1 != null && itemstack1.getItem() == par1ItemStack.getItem()
+            && (!par1ItemStack.getHasSubtypes() || par1ItemStack.getItemDamage() == itemstack1.getItemDamage())
+            && ItemStack.areItemStackTagsEqual(par1ItemStack, itemstack1) && slot.isItemValid(par1ItemStack) && par1ItemStack != itemstack1) {
+
+          int mergedSize = itemstack1.stackSize + par1ItemStack.stackSize;
+          int maxStackSize = Math.min(par1ItemStack.getMaxStackSize(), slot.getSlotStackLimit());
+          if (mergedSize <= maxStackSize) {
+            par1ItemStack.stackSize = 0;
+            itemstack1.stackSize = mergedSize;
+            slot.onSlotChanged();
+            result = true;
+          } else if (itemstack1.stackSize < maxStackSize) {
+            par1ItemStack.stackSize -= maxStackSize - itemstack1.stackSize;
+            itemstack1.stackSize = maxStackSize;
+            slot.onSlotChanged();
+            result = true;
+          }
+        }
+
+        if (reversOrder) {
+          --checkIndex;
+        } else {
+          ++checkIndex;
+        }
+      }
+    }
+
+    if (par1ItemStack.stackSize > 0) {
+      if (reversOrder) {
+        checkIndex = toIndex - 1;
+      } else {
+        checkIndex = fromIndex;
+      }
+
+      while (!reversOrder && checkIndex < toIndex || reversOrder && checkIndex >= fromIndex) {
+        slot = this.inventorySlots.get(checkIndex);
+        itemstack1 = slot.getStack();
+
+        if (itemstack1 == null && slot.isItemValid(par1ItemStack)) {
+          ItemStack in = par1ItemStack.copy();
+          in.stackSize = Math.min(in.stackSize, slot.getSlotStackLimit());
+
+          slot.putStack(in);
+          slot.onSlotChanged();
+          if (in.stackSize >= par1ItemStack.stackSize) {
+            par1ItemStack.stackSize = 0;
+          } else {
+            par1ItemStack.stackSize -= in.stackSize;
+          }
+          result = true;
+          break;
+        }
+
+        if (reversOrder) {
+          --checkIndex;
+        } else {
+          ++checkIndex;
+        }
+      }
+    }
+
+    return result;
+  }
+  
+ 
+}
