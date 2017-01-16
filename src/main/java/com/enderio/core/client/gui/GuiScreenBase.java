82d83a1e75f8ac772f9ef088d9f174975bc5e149
Henry Loenwind <henry@loenwind.info>
2017-01-16 21:41:15 +0100

    Removed long outdated code

--- src/main/java/com/enderio/core/client/gui/GuiScreenBase.java
+++ src/main/java/com/enderio/core/client/gui/GuiScreenBase.java
@@ -1,7 +1,6 @@
 package com.enderio.core.client.gui;
 
 import java.io.IOException;
-import java.util.Iterator;
 import java.util.List;
 
 import com.enderio.core.api.client.gui.IGuiScreen;
@@ -103,81 +102,9 @@ public abstract class GuiScreenBase extends GuiScreen implements ToolTipRenderer
   protected void drawForegroundImpl(int mouseX, int mouseY) {
   }
 
-  @SuppressWarnings("rawtypes")
   @Override
-  public void drawHoveringText(List par1List, int par2, int par3, FontRenderer font) {
-
-    if (!par1List.isEmpty()) {
-
-      GlStateManager.disableNormalize();
-
-      RenderHelper.disableStandardItemLighting();
-      GlStateManager.disableLighting();
-      GlStateManager.disableDepth();
-
-      int k = 0;
-      Iterator iterator = par1List.iterator();
-
-      while (iterator.hasNext()) {
-        String s = (String) iterator.next();
-        int l = font.getStringWidth(s);
-
-        if (l > k) {
-          k = l;
-        }
-      }
-
-      int i1 = par2 + 12;
-      int j1 = par3 - 12;
-      int k1 = 8;
-
-      if (par1List.size() > 1) {
-        k1 += 2 + (par1List.size() - 1) * 10;
-      }
-
-      if (i1 + k > width) {
-        i1 -= 28 + k;
-      }
-
-      if (j1 + k1 + 6 > height) {
-        j1 = height - k1 - 6;
-      }
-
-      zLevel = 300.0F;
-
-      int l1 = -267386864;
-      drawGradientRect(i1 - 3, j1 - 4, i1 + k + 3, j1 - 3, l1, l1);
-      drawGradientRect(i1 - 3, j1 + k1 + 3, i1 + k + 3, j1 + k1 + 4, l1, l1);
-      drawGradientRect(i1 - 3, j1 - 3, i1 + k + 3, j1 + k1 + 3, l1, l1);
-      drawGradientRect(i1 - 4, j1 - 3, i1 - 3, j1 + k1 + 3, l1, l1);
-      drawGradientRect(i1 + k + 3, j1 - 3, i1 + k + 4, j1 + k1 + 3, l1, l1);
-      int i2 = 1347420415;
-      int j2 = (i2 & 16711422) >> 1 | i2 & -16777216;
-      drawGradientRect(i1 - 3, j1 - 3 + 1, i1 - 3 + 1, j1 + k1 + 3 - 1, i2, j2);
-      drawGradientRect(i1 + k + 2, j1 - 3 + 1, i1 + k + 3, j1 + k1 + 3 - 1, i2, j2);
-      drawGradientRect(i1 - 3, j1 - 3, i1 + k + 3, j1 - 3 + 1, i2, i2);
-      drawGradientRect(i1 - 3, j1 + k1 + 2, i1 + k + 3, j1 + k1 + 3, j2, j2);
-
-      for (int k2 = 0; k2 < par1List.size(); ++k2) {
-        String s1 = (String) par1List.get(k2);
-        font.drawStringWithShadow(s1, i1, j1, -1);
-
-        if (k2 == 0) {
-          j1 += 2;
-        }
-
-        j1 += 10;
-      }
-
-      zLevel = 0.0F;
-
-      GlStateManager.enableLighting();
-      GlStateManager.enableDepth();
-
-      RenderHelper.enableStandardItemLighting();
-      GlStateManager.enableNormalize();
-
-    }
+  public void drawHoveringToolTipText(List<String> par1List, int par2, int par3, FontRenderer font) {
+    super.drawHoveringText(par1List, par2, par3, font);
   }
 
   @Override
