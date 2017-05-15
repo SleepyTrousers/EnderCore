f9b030006d29cee075513b5ed5fe1b73eb579d07
Henry Loenwind <henry@loenwind.info>
2017-05-15 06:42:14 +0200

    [1.11.2] Adaptions to EIO changes

--- src/main/java/com/enderio/core/common/util/UserIdent.java
+++ src/main/java/com/enderio/core/common/util/UserIdent.java
@@ -35,6 +35,10 @@ public class UserIdent {
     return uuid != null ? uuid + "" : NONE_MARKER;
   }
 
+  public @Nonnull GameProfile getAsGameProfile() {
+    return new GameProfile(getUUID(), getPlayerName());
+  }
+
   /**
    * Create a UserIdent from a UUID object and a name. Use this when reading stored data, it will check for username changes, implement them and write a log
    * message.
