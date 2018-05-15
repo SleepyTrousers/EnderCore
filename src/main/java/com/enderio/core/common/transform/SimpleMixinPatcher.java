7f2d3785a1abe696af7fa9a79cef3ab4cf136cc5
tterrag <tterrag1098@gmail.com>
2018-05-15 13:43:52 -0400

    Fix issues with SimpleMixin and invocations of member methods

--- src/main/java/com/enderio/core/common/transform/SimpleMixinPatcher.java
+++ src/main/java/com/enderio/core/common/transform/SimpleMixinPatcher.java
@@ -1,8 +1,13 @@
 package com.enderio.core.common.transform;
 
+import static org.objectweb.asm.ClassWriter.COMPUTE_FRAMES;
+import static org.objectweb.asm.ClassWriter.COMPUTE_MAXS;
 import static org.objectweb.asm.Opcodes.ACC_ABSTRACT;
+import static org.objectweb.asm.Opcodes.INVOKEINTERFACE;
+import static org.objectweb.asm.Opcodes.INVOKEVIRTUAL;
 
 import java.util.HashMap;
+import java.util.ListIterator;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -10,7 +15,11 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.tree.AbstractInsnNode;
 import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.LocalVariableNode;
+import org.objectweb.asm.tree.MethodInsnNode;
+import org.objectweb.asm.tree.MethodNode;
 
 import com.enderio.core.common.transform.EnderCorePlugin.InterfacePatchData;
 
@@ -50,13 +59,38 @@ public class SimpleMixinPatcher implements IClassTransformer {
         ClassReader sourceReader = new ClassReader(sourceClass);
         sourceReader.accept(sourceNode, 0);
         
-        targetNode.interfaces.addAll(sourceNode.interfaces);
+        for (MethodNode m : sourceNode.methods) {
+          ListIterator<AbstractInsnNode> instructions = m.instructions.iterator();
+          while (instructions.hasNext()) {
+            AbstractInsnNode node = instructions.next();
+            if (node instanceof MethodInsnNode) {
+              MethodInsnNode call = (MethodInsnNode) node;
+              if (call.owner.replace('/', '.').equals(data.source)) {
+                call.owner = data.target.replace('.', '/');
+                if (call.getOpcode() == INVOKEINTERFACE) {
+                  call.setOpcode(INVOKEVIRTUAL);
+                  call.itf = false;
+                }
+              }
+            }
+          }
+          
+          if (m.localVariables.size() > 0) {
+            LocalVariableNode n = m.localVariables.get(0);
+            n.desc = "L" + data.target.replace('.', '/') + ";";
+          }
+        }
+        
+        targetNode.interfaces.addAll(sourceNode.interfaces.stream()
+            .filter(s -> !targetNode.interfaces.contains(s))
+            .collect(Collectors.toList()));
+        
         targetNode.methods.addAll(sourceNode.methods.stream()
             .filter(m -> !m.name.equals("<init>"))
             .filter(m -> (m.access & ACC_ABSTRACT) == 0)
             .collect(Collectors.toList()));
 
-        ClassWriter cw = new ClassWriter(0);
+        ClassWriter cw = new ClassWriter(COMPUTE_MAXS | COMPUTE_FRAMES);
         targetNode.accept(cw);
         logger.info("Patched interfaces from {} onto {}.", data.source, data.target);
         return cw.toByteArray();
