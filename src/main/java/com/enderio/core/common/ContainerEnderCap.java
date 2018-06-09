ae9dcdc2d1f3d5198a566e70432e82da9c96c869
EpicSquid <epicsquid315@gmail.com>
2018-06-09 23:25:26 +1000

    Added the ability to get Ender Slots for gui usage

--- src/main/java/com/enderio/core/common/ContainerEnderCap.java
+++ src/main/java/com/enderio/core/common/ContainerEnderCap.java
@@ -7,6 +7,7 @@ import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
 import com.enderio.core.client.gui.widget.GhostSlot;
+import com.enderio.core.common.inventory.EnderSlot;
 import com.google.common.collect.Maps;
 
 import net.minecraft.entity.player.EntityPlayer;
@@ -25,6 +26,9 @@ import net.minecraftforge.items.IItemHandler;
 public abstract class ContainerEnderCap<T extends IItemHandler, S extends TileEntity> extends Container implements GhostSlot.IGhostSlotAware {
 
   protected final @Nonnull Map<Slot, Point> slotLocations = Maps.newLinkedHashMap();
+
+  // TODO Make a wrapper for normal slots to be EnderSlots
+  protected final @Nonnull Map<EnderSlot, Point> enderSlotLocations = Maps.newLinkedHashMap();
 
   protected int startPlayerSlot;
   protected int endPlayerSlot;
@@ -81,6 +85,12 @@ public abstract class ContainerEnderCap<T extends IItemHandler, S extends TileEn
 
     initRan = true;
     return (X) this;
+  }
+
+  @Nonnull
+  public Slot addEnderSlotToContainer(@Nonnull EnderSlot slotIn) {
+    enderSlotLocations.put(slotIn, new Point(slotIn.xPos, slotIn.yPos));
+    return addSlotToContainer(slotIn);
   }
   
   @Override
@@ -252,6 +262,11 @@ public abstract class ContainerEnderCap<T extends IItemHandler, S extends TileEn
         }
       }
     }
+  }
+
+  @Nonnull
+  public Map<EnderSlot, Point> getEnderSlotLocations() {
+    return enderSlotLocations;
   }
 
 }
