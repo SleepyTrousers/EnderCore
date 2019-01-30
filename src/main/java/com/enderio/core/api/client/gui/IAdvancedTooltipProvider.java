30dde18d17c9fab466223176491d757d9eace5a0
Henry Loenwind <henry@loenwind.info>
2019-01-30 20:59:06 +0100

    Made tooltip API use default methods to allow them to be provided by a mixin

--- src/main/java/com/enderio/core/api/client/gui/IAdvancedTooltipProvider.java
+++ src/main/java/com/enderio/core/api/client/gui/IAdvancedTooltipProvider.java
@@ -13,12 +13,15 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 public interface IAdvancedTooltipProvider {
 
   @SideOnly(Side.CLIENT)
-  public void addCommonEntries(@Nonnull ItemStack itemstack, @Nullable EntityPlayer entityplayer, @Nonnull List<String> list, boolean flag);
+  default void addCommonEntries(@Nonnull ItemStack itemstack, @Nullable EntityPlayer entityplayer, @Nonnull List<String> list, boolean flag) {
+  }
 
   @SideOnly(Side.CLIENT)
-  public void addBasicEntries(@Nonnull ItemStack itemstack, @Nullable EntityPlayer entityplayer, @Nonnull List<String> list, boolean flag);
+  default void addBasicEntries(@Nonnull ItemStack itemstack, @Nullable EntityPlayer entityplayer, @Nonnull List<String> list, boolean flag) {
+  }
 
   @SideOnly(Side.CLIENT)
-  public void addDetailedEntries(@Nonnull ItemStack itemstack, @Nullable EntityPlayer entityplayer, @Nonnull List<String> list, boolean flag);
+  default void addDetailedEntries(@Nonnull ItemStack itemstack, @Nullable EntityPlayer entityplayer, @Nonnull List<String> list, boolean flag) {
+  }
 
 }
